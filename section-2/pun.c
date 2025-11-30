655d12ec678b5c37b9e8385dc9c21b10c790ebb1
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-11-30 15:41:40 +0800

    doc: Add comments

--- section-2/pun.c
+++ section-2/pun.c
@@ -1,6 +1,14 @@
+/* 
+    Name: pun.c
+    Purpose: Print a bad pun.
+    Author: Jaizzer
+
+*/
 #include <stdio.h>
 
 int main (void) {
+    // Print the pun
     printf("To C, or not to C: that is the question.\n");
+    
     return 0;
 }
