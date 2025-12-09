506574c52a2aa7a70c404b4d3f9f38131c43f297
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-12-09 13:33:04 +0800

    feat: Create code for analyzing how white spaces are handled by the scanf

---
+++ section-3/scanf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    int b;
+    float c;
+    float d;
+
+    printf("Inputs: ");
+    scanf("%d%d%f%f", &a, &b, &c, & d);
+    printf("%d %d %f %f\n", a, b, c, d);
+
+    return 0;
+}
