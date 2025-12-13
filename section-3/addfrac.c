0501442eebb10a256993e7d728d7f25b0c548d1c
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-12-14 00:02:45 +0800

    feat: Modify the addfrac program to write the faction addition expression in one line

--- section-3/addfrac.c
+++ section-3/addfrac.c
@@ -2,11 +2,8 @@
 
 int main(void) {
     int numerator1, denominator1, numerator2, denominator2;
-    printf("Enter fraction 1: ");
-    scanf("%d/%d", &numerator1, &denominator1);
-
-    printf("Enter fraction 2: ");
-    scanf("%d/%d", &numerator2, &denominator2);
+    printf("Enter fraction addition expression: ");
+    scanf("%d/%d+%d/%d", &numerator1, &denominator1, &numerator2, &denominator2);
 
     int numerator = numerator1 * denominator2 + numerator2 * denominator1;
     int denominator = denominator1 * denominator2;
